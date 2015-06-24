d521020a7a4ed1b66d3a7e9bc528db57a258e3c4
dobiedad <leomdiv@gmail.com>
2015-06-24 16:10:35 +0100

    pages + smoothscroll

--- public/bundle.js
+++ public/bundle.js
@@ -2,15 +2,55 @@
 var plastiq = require('plastiq');
 var h = plastiq.html;
 
+function smoothScroll(ev) {
+  var target = $(ev.target);
+  var body = $('body');
+  var scrollTo = $(target.attr('href')).offset().top;
+  $('html, body').animate({ scrollTop: scrollTo},800);
+  ev.preventDefault();
+  return false;
+};
+
 function render(model) {
   return h('div',
-    h('label', "what's your name?"), ' ',
-    h('input', {type: 'text', binding: [model, 'name']}),
-    h('div', 'hi ', model.name)
+    h('header#nav',
+      h('a.logo',
+        h('i.fa.fa-money'),
+        h('span', 'My Business')
+      ),
+      h('div#nav-links',
+        h('a',{href: '#home' , onclick: smoothScroll }, 'Home'),
+        h('a',{href: '#about', onclick: smoothScroll  }, 'About'),
+        h('a',{href: '#products', onclick: smoothScroll  }, 'Products'),
+        h('a',{href: '#contact', onclick: smoothScroll  }, 'Contact')
+      )
+    ),
+    h('div#home.section',
+      h('div#banner'),
+      h('div.overlay'),
+      h('div#banner-title',
+        h('h1', 'Welcome!'),
+        h('div.button', 'Explore')
+      )
+    ),
+    h('div#about.section',
+      h('h1','about'),
+      h('div.overlay')
+    ),
+    h('div#products.section',
+      h('h1','products'),
+      h('div.overlay')
+    ),
+    h('div#contact.section',
+      h('h1','contact'),
+      h('div.overlay')
+    )
   );
 }
 
-plastiq.append(document.body, render, {name: ''});
+plastiq.append(document.body, render, {
+  name: ''
+});
 
 },{"plastiq":6}],2:[function(require,module,exports){
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express()
 var morgan = require('morgan');
+var jquery = require('jquery');
+
 app.use(morgan('combined'))
 // respond with "hello world" when a GET request is made to the homepage
 app.use(express.static('public'));
